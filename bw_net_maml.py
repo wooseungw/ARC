d616fdf44a19dd1a9b75c2b6cc96d6472ba7ee57
ByungWanLim <pook0612@naver.com>
2024-09-06 20:40:52 +0900

    163163613

--- bw_net_maml.py
+++ bw_net_maml.py
@@ -8,7 +8,8 @@ class FEBlock(nn.Module):
         # 1x1 ~ 30x30 Convolution layers 생성 (Padding X)
         # self.convs = nn.ModuleList([nn.Conv2d(1, embed_size, kernel_size=n, padding=0) for n in range(1, 31)])
         # self.fc = nn.ModuleList([nn.Linear((30-n)**(30-n)embed_size, 30*30*embed_size) for n in range(1, 31)])
-        self.numbers = [1, 2, 3, 5, 7, 9, 11, 13, 15, 25, 30]
+        # self.numbers = [1, 2, 3, 5, 7, 9, 11, 13, 15, 25, 30]
+        self.numbers = [1, 2, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 24, 26, 30]
         self.stages = nn.ModuleList([])
         for n in self.numbers:
             self.stages.append(nn.Sequential(
@@ -51,17 +52,22 @@ class SelfAttentionBlock(nn.Module):
     def __init__(self, embed_size=1):
         super(SelfAttentionBlock, self).__init__()
         self.cls_token = nn.Parameter(torch.randn(1, 1, 30*30*embed_size))
+        self.layernorm1 = nn.LayerNorm(30*30*embed_size)
         self.self_attn = nn.MultiheadAttention(embed_dim=30*30*embed_size, num_heads=4)
 
     def forward(self, x):
         batch_size = x.size(0)
         cls_tokens = self.cls_token.expand(batch_size, -1, -1)
-        # print(cls_tokens.shape)
-        # print(x.shape)
+        #print(cls_tokens.shape)
+        #print(x.shape)
         x = x.squeeze(2)
-        # print(x.shape)
+        #print(x.shape)
         x = torch.cat((cls_tokens, x), dim=1)  # (batch, 31, 30*30*embed_size)
+        #print(x.shape)
+        x = self.layernorm1(x)
+        #print(x.shape)
         x, _ = self.self_attn(x, x, x)
+        #print(x.shape)
         return x[:, 0].unsqueeze(1)  # (batch, 1, 30*30*embed_size)
 
 class HeadBlock(nn.Module):
@@ -75,7 +81,9 @@ class HeadBlock(nn.Module):
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
+        x = nn.Dropout(0.2)(x)
         x = F.relu(self.fc2(x))
+        x = nn.Dropout(0.2)(x)
         x = self.fc3(x)
         x = x.view(-1, 1, 30, 30)  # Reshape to (batch, 1, 30, 30)
         x = self.conv(x)
@@ -94,14 +102,14 @@ class BWNet_MAML(nn.Module):
         features = self.fe_block(ex_input)  # (batch, 30, 30*30*embed_size)
         cls_feature = self.self_attn_block(features)  # (batch, 1, 30*30*embed_size)
         out = self.head_block(cls_feature)  # (batch, 1, 30, 30)
-        #out = self.fc_final(out)  # (batch, 11, 30, 30)
+        # out = self.fc_final(out)  # (batch, 11, 30, 30)
         return out
 
 # input_tensor = torch.randn(1, 1, 30, 30)  # 입력 텐서 예시
 # example_input = torch.randn(10, 1, 30, 30)  # 입력 텐서 예시
 # # example_output = torch.randn(10, 1, 30, 30)  # 출력 텐서 예시
 
-# model = BWNet_MAML(embed_size=2)
+# model = BWNet_MAML(embed_size=1)
 # output = model(example_input)
 
 # print(output.shape)  # 최종 출력 크기를 확인
